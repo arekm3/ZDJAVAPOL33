d500618df0399d1a1e64fe861138934510402969
Arek <arekmontsko@gmail.com>
2020-09-05 00:07:51 +0200

    adding a fifth task

--- src/exercises/Ex5.java
+++ src/exercises/Ex5.java
@@ -1,2 +1,23 @@
+package exercises;
+
 public class Ex5 {
+    public static void main(String[] args) {
+        int[] a = {23, 235, 453, 3, 4};
+        showBiggestAndSmallestFromTable(a);
+    }
+
+    public static void showBiggestAndSmallestFromTable(int[] numbers) {
+        if (numbers.length < 1) {
+            System.out.println("Array is empty");
+        } else {
+            int min = numbers[0];
+            int max = min;
+            for (int number : numbers) {
+                if (min > number) min = number;
+                if (max < number) max = number;
+            }
+            System.out.println("Max: " + max + " min: " + min);
+        }
+    }
 }
+
