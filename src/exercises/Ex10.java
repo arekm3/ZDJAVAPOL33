bafbc25410ce120fd8984b5a86d51d76ced2636e
Arek <arekmontsko@gmail.com>
2020-09-05 00:12:33 +0200

    adding the tenth task

--- src/exercises/Ex10.java
+++ src/exercises/Ex10.java
@@ -1,4 +1,24 @@
 package exercises;
 
-public class Ex10 {
+public class Ex10 extends Ex9 {
+    public static void separator() {
+        System.out.println("-------------");
+    }
+
+    public static void buildInModulo(int dividend, int divider) {
+        System.out.println(dividend % divider);
+    }
+
+    public static void main(String[] args) {
+        printModulo(7, 5);
+        printModulo(11,2);
+
+        separator();
+
+        buildInModulo(7,5);
+        buildInModulo(11,2);
+
+
+
+    }
 }
