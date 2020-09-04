cf6d2b50ea9eb4af2883287c9f49d044a131fbb7
Arek <arekmontsko@gmail.com>
2020-09-05 00:05:23 +0200

    adding a third task

--- src/exercises/Ex3.java
+++ src/exercises/Ex3.java
@@ -1,4 +1,15 @@
 package exercises;
 
 public class Ex3 {
+    public static void main(String[] args) {
+        System.out.println("Suma podanych liczb to: " + sumFromXtoY(5, 10));
+    }
+
+    public static int sumFromXtoY(int x, int y) {
+        int suma = 0;
+        for (int i = x; i <= y; i++) {
+            suma += i;
+        }
+        return suma;
+    }
 }
