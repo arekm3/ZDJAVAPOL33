0cbdf07eac88d764537b5a7a54c491e53cb6d6db
Arek <arekmontsko@gmail.com>
2020-09-05 00:06:33 +0200

    adding a fourth task

--- src/exercises/Ex4.java
+++ src/exercises/Ex4.java
@@ -1,4 +1,24 @@
 package exercises;
 
+import java.util.Arrays;
+
 public class Ex4 {
+    public static void main(String[] args) {
+        int[] a = {12, 7, 9};
+
+        a = swapFirstToThird(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static int[] swapFirstToThird(int[] numbers) {
+        int buf;
+        if (numbers.length < 3) {
+            return numbers;
+        }
+        buf = numbers[0];
+        numbers[0] = numbers[2];
+        numbers[2] = buf;
+        return numbers;
+    }
 }
+
